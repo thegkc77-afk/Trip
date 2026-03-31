9fd0159579dfe6cfffdda47619f5e21daa834827
[firstname lastname] <thegkc77@gmail.com>
2026-03-31 14:00:44 +0530

    all deleted1

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,12 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
-
-import HeroSection from './Pages/HeroSection'
-import Navbar from './Pages/Navbar'
-import Howitwork from './Pages/Howitwork'
-import About from './components/About'
-
-
-function Home(){
-  return (
-    <>
-    <HeroSection/>
-    <Howitwork/>
-    <About/>
-   
-    </>
-
-  )
-}
-
-
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <div className='flex flex-col min-h-screen'>
-        <Navbar/>
-        <main className='flex-1'>
-
-          <Routes>
-          <Route path='/' element={<Home/>}/>
-
-          </Routes>
-        </main>
-      </div>
-
-
-
-
-
-    </BrowserRouter>
+    <h1>New</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, magni!</p>
+   
      
     
      
